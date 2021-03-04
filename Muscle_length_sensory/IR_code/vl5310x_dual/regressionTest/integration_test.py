63eb85c76d7eaec8fbf2c7c92c121829a4f6f868
Jake Chung <luonchung@gmail.com>
2021-03-03 21:59:27 -0800

    integration test to test readData and plottingRoutine

--- Muscle_length_sensory/IR_code/vl5310x_dual/regressionTest/integration_test.py
+++ Muscle_length_sensory/IR_code/vl5310x_dual/regressionTest/integration_test.py
@@ -4,16 +4,18 @@ import numpy as np
 
 
 # test integration of the classes
-path = {'raw 1': 'rawData3.txt',
-            'raw 2': 'rawData4.txt',
-            'raw 3': 'rawData5.txt',
-            'raw 4': 'rawData6.txt',
-            'raw 5 static:': 'rawData7_static.txt',
-            'raw 6 static:': 'rawData8_static.txt',
-            'raw 7 static:': 'rawData9_static.txt'}
+path = {'R1 dynamic BPA': 'rawData3.txt',
+        'R2 dynamic BPA': 'rawData4.txt',
+        'R3 dynamic BPA': 'rawData5.txt',
+        'R4 dynamic BPA': 'rawData6.txt',
+        'R5 static': 'rawData7_static.txt',
+        'R6 some static': 'rawData8_static.txt',
+        'R7 some static': 'rawData9_static.txt'}
 
 # instantiate the data
 data = readData.Data(path)
 # put the data into a 3D array
 rawArr = data.readData2Array()
-makePlots = visualData.plottingRoutine(rawArr)
+keys = data.returnKeys()
+makePlots = visualData.plottingRoutine(rawArr, keys)
+makePlots.plotSeries()

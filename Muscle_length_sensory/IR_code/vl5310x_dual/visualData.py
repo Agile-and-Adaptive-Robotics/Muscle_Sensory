350888cd511e7b92e02e337060f5b30e9f48d37b
Jake Chung <luonchung@gmail.com>
2021-03-03 22:17:22 -0800

    add logic to show plots, add option to plot delta

--- Muscle_length_sensory/IR_code/vl5310x_dual/visualData.py
+++ Muscle_length_sensory/IR_code/vl5310x_dual/visualData.py
@@ -8,7 +8,7 @@ class plottingRoutine(object):
     This class does all kind of plotting depending on the method
     """
 
-    def __init__(self, data, keys):
+    def __init__(self, data, keys, showPlots=False):
         """
         initialize input data. Expect a 3D array
         :param data:
@@ -19,25 +19,48 @@ class plottingRoutine(object):
         print('-> Create plotting routine object.')
         self.data = data
         self.keys = keys
+        self.showPlots = showPlots
+        # define sampling period to make time array
+        self.tPeriod = 0.093
+        # get number of files
+        self.z_len = len(self.data[0, 0, :])
 
-    def plotSeries(self):
+    def plotTimeSeries(self, showPlots=False):
         """
         method to plot time series, input is time array
         :return:
         """
-        # define sampling period to make time array
-        tPeriod = 0.093
-        # get number of files
-        z_len = len(self.data[0, 0, :])
+        print('>> Plot time series.')
         # create control array for for-loop
-        controlArr = np.arange(z_len)
+        controlArr = np.arange(self.z_len)
         for counter in controlArr:
-            time = np.linspace(0, tPeriod * len(self.data[:, 0, 0]), len(self.data[:, 0, 0]))
+            time = np.linspace(0, self.tPeriod * len(self.data[:, 0, 0]), len(self.data[:, 0, 0]))
             fig, axes = plt.subplots(nrows=1, ncols=1, figsize=(12, 5))
             axes.plot(time, self.data[:, 0, counter])
             axes.plot(time, self.data[:, 1, counter])
             axes.set_ylabel("Distance(mm)")
             axes.set_xlabel("Time (s)")
             axes.set_title(str(self.keys[counter]) + ' Plot')
+        if showPlots:
+            plt.show()
 
+    def plotDiff(self, showPlots=False):
+        """
+        method to plot delta of the 2 sensors, input is time array
+        :return:
+        """
+        print('>> Plot delta between sensors.')
+        # create control array for for-loop
+        controlArr = np.arange(self.z_len)
+        for counter in controlArr:
+            time = np.linspace(0, self.tPeriod * len(self.data[:, 0, 0]), len(self.data[:, 0, 0]))
+            fig, axes = plt.subplots(nrows=1, ncols=1, figsize=(12, 5))
+            axes.plot(time, self.data[:, 0, counter] - self.data[:, 1, counter])
+            axes.set_ylabel("Delta (mm)")
+            axes.set_xlabel("Time (s)")
+            axes.set_title(str(self.keys[counter]) + ' Plot')
+        if showPlots:
+            plt.show()
+
+    def showAllPlots(self):
         plt.show()

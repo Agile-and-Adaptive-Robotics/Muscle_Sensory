156bafba2c0f41b4d4e289a57922d8540504854c
Jake Chung <luonchung@gmail.com>
2021-03-06 17:34:41 -0800

    add method to do ladder plot

--- Muscle_length_sensory/IR_code/vl5310x_dual/integration_test.py
+++ Muscle_length_sensory/IR_code/vl5310x_dual/integration_test.py
@@ -13,13 +13,13 @@ path = {'R1 dynamic BPA': 'rawData3.txt',
         'R6 some static': 'rawData8_static.txt',
         'R7 some static': 'rawData9_static.txt'}
 
-
 # instantiate the data
 data = readData.Data(path)
 # put the data into a 3D array
 rawArr = data.readData2Array()
 keys = data.returnKeys()
 makePlots = visualData.plottingRoutine(path)
-makePlots.plotTimeSeries(specificPlots=(5, 6))
-makePlots.plotDiff(showPlots=False, specificPlots=(5, 6))
+# makePlots.plotTimeSeries(specificPlots=(5, 6))
+# makePlots.plotDiff(showPlots=False, specificPlots=(5, 6))
+makePlots.plotSeriesLadder(specificPlots=(4, 5, 6))
 makePlots.showAllPlots()
--- Muscle_length_sensory/IR_code/vl5310x_dual/visualData.py
+++ Muscle_length_sensory/IR_code/vl5310x_dual/visualData.py
@@ -11,13 +11,15 @@ class plottingRoutine(Data):
 
     # TODO: consider using bokeh for plotting
 
-    def __init__(self, pathtoFile, showPlots=False):
+    def __init__(self, pathToFile, showPlots=False):
         """
         initialize input data. Expect a 3D array
         :param data:
         """
+        if pathToFile is None:
+            Exception('Need to give path to files')
         print('-> Create plotting routine object.')
-        super().__init__(pathToFile=pathtoFile)
+        super().__init__(pathToFile=pathToFile)
         self.data = super().readData2Array()
         self.keys = super().returnKeys()
         self.showPlots = showPlots
@@ -81,6 +83,37 @@ class plottingRoutine(Data):
                 axes.set_ylabel("Delta (mm)")
                 axes.set_xlabel("Time (s)")
                 axes.set_title(str(self.keys[counter]) + ' Plot')
+        if showPlots:
+            plt.show()
+
+    def plotSeriesLadder(self, showPlots=False, specificPlots=None):
+        """
+        Method to do ladder plot
+        :return:
+        """
+        print(">> Generate ladder plot.")
+        # create control array for for-loop
+        controlArr = np.arange(self.z_len)
+        assert isinstance(specificPlots, tuple), 'Plot selection must be a tuple'
+        if specificPlots is None:
+            fig, axes = plt.subplots(nrows=len(controlArr), ncols=1, figsize=(12, 5))  # generate template
+            for counter in controlArr:
+                time = np.linspace(0, self.tPeriod * len(self.data[:, 0, 0]), len(self.data[:, 0, 0]))
+                axes[counter].plot(time, self.data[:, 0, counter])
+                axes[counter].plot(time, self.data[:, 1, counter])
+                axes[counter].set_ylabel("Distance(mm)")
+                axes[counter].set_xlabel("Time (s)")
+                axes[counter].set_title(str(self.keys[counter]) + ' Plot')
+        else:
+            fig, axes = plt.subplots(nrows=len(specificPlots), ncols=1, figsize=(12, 5))  # generate template
+            axes[0].set_title('Plot ' + str(specificPlots))
+            for counter in specificPlots:
+                time = np.linspace(0, self.tPeriod * len(self.data[:, 0, 0]), len(self.data[:, 0, 0]))
+                axes[specificPlots.index(counter)].plot(time, self.data[:, 0, counter])
+                axes[specificPlots.index(counter)].plot(time, self.data[:, 1, counter])
+                axes[specificPlots.index(counter)].set_ylabel("Distance(mm)")
+                axes[specificPlots.index(counter)].set_xlabel("Time (s)")
+
         if showPlots:
             plt.show()
 

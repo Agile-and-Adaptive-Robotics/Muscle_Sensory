73676f9741ec43f40336dc51da6577bee7dfe1e4
Jake Chung <luonchung@gmail.com>
2021-06-26 19:04:13 -0700

    working implementation for SD card data logging with interrupt for timing

--- Muscle_length_sensory/IR_code/Teensy_vl5310x_SD_datalogger/Teensy_vl5310x_SD_datalogger.ino
+++ Muscle_length_sensory/IR_code/Teensy_vl5310x_SD_datalogger/Teensy_vl5310x_SD_datalogger.ino
@@ -11,9 +11,12 @@ June 26 2021
 // include libraries for communicating with the VL53L0X
 #include <Wire.h>
 #include <VL53L0X.h>
-// include the SD library:
+// include the SD library
 #include <SD.h>
 #include <SPI.h>
+// include interrupt library
+#include <avr/io.h>
+#include <avr/interrupt.h>
 
 // set up variables using the SD utility library functions:
 Sd2Card card;
@@ -23,7 +26,15 @@ SdFile root;
 // set up the sensor
 VL53L0X sensor;
 
+// create interrupt function
+IntervalTimer myTimer; 
+
 const int chipSelect = BUILTIN_SDCARD;
+float Ts_micro;
+float samplingFreq = 50; //define sampling frequency in Hz
+float collectPeriod = 10; //number of seconds to collect data
+volatile float stopCount; //how mane counts to stop counting given sampling Frequency
+volatile int counter = 0; //variable to count about many times interrupt was fired
 // String IR_reading;
 // File dataFile
 
@@ -58,10 +69,44 @@ void setup()
         while (1) {}
     }
     Serial.println("SD card is ready!");
+
+    //Write header file
+    File dataFile = SD.open("datalog.txt", FILE_WRITE);
+    if (!dataFile){
+        Serial.println("Failed to open file to write header.");
+        while (1) {}
+    }
+    if (dataFile){
+        dataFile.println("=====Start_file_header=====");
+        dataFile.print("Collection period (s): ");
+        dataFile.print(collectPeriod);
+        dataFile.print(", Sampling Frequency (Hz): ");
+        dataFile.println(samplingFreq);
+        dataFile.println("Counter, Distance(mm)");
+        dataFile.close();
+    }
+
+    // init interrupt
+    Serial.println("Initialize interrupt routine.");
+    Ts_micro = 1/samplingFreq * 1000000.0; //sampling in microseconds
+    myTimer.begin(DataLoggingLoop, Ts_micro); //control loop to run at 1kHz
+    Serial.println("Interrupt is ready!");
+    stopCount = floor(collectPeriod/(1/samplingFreq));
+    Serial.print("Stop count: ");
+    Serial.println(stopCount);
 }
 
 void loop()
 {
+    //do nothing
+}
+
+void DataLoggingLoop()
+{
+    if (counter > stopCount) { //stop data collection
+        Serial.println("Completed data collection!");
+        return;
+    }
     String IR_reading = String(sensor.readRangeContinuousMillimeters());
     if (sensor.timeoutOccurred()) { Serial.print(" TIMEOUT"); }
     //open file 
@@ -71,10 +116,11 @@ void loop()
         while (1) {}
     }
     if (dataFile){
+        dataFile.print(counter);
+        dataFile.print(", ");
         dataFile.println(IR_reading);
         dataFile.close();
-        // write to Serial. Comment out when run for real
-        Serial.println(IR_reading); 
-
+        Serial.println(counter); // write to Serial. Comment out when run for real
     }
+    counter += 1;
 }

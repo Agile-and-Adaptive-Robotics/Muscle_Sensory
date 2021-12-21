54a2abfc0ed8d96f7519cf37a0e02904b6fca185
Lawrence Pang <lawrence_91096@hotmail.com>
2021-12-21 11:37:06 -0800

    DataAnalysisfor10mmData

---
+++ DataCollection/2021Dec16_10mmKinked/ConvertmattoCsv.m
@@ -0,0 +1,15 @@
+%Loading data from mat file and save as csv.
+%Kinked4mm = load('KinkedData_0_4cm.mat');
+% Kinked12mm = load('KinkedData_1_2cm.mat');
+% csvwrite('Kinked_12mmTest1Data.csv',Kinked12mm.Test1Data);
+% csvwrite('Kinked_12mmTest2Data.csv',Kinked12mm.Test2Data);
+% csvwrite('Kinked_12mmTest3Data.csv',Kinked12mm.Test3Data);
+% csvwrite('Kinked_12mmTest4Data.csv',Kinked12mm.Test4Data);
+% csvwrite('Kinked_12mmTest5Data.csv',Kinked12mm.Test5Data);
+% csvwrite('Kinked_12mmTest6Data.csv',Kinked12mm.Test6Data);
+% csvwrite('Kinked_12mmTest7Data.csv',Kinked12mm.Test7Data);
+% csvwrite('Kinked_12mmTest8Data.csv',Kinked12mm.Test8Data);
+% csvwrite('Kinked_12mmTest9Data.csv',Kinked12mm.Test9Data);
+% csvwrite('Kinked_12mmTest10Data.csv',Kinked12mm.Test10Data);
+
+

cfb41292d09b769438621a73b79276b79e6c121b
Lawrence Pang <lawrence_91096@hotmail.com>
2022-01-04 00:00:50 -0800

    10mm 30cm Data

---
+++ rundata.m
@@ -0,0 +1,12 @@
+function rundata
+[Offset,OffsetStat] = ValvePWM(2,5,29000,0,500,400,500);
+[Test1Data,Test1Stat]=ValvePWM(2,5,29000,10,500,470,500);
+[Test2Data,Test2Stat]=ValvePWM(2,5,29000,10,500,470,500);
+[Test3Data,Test3Stat]=ValvePWM(2,5,29000,10,500,460,500);
+[Test4Data,Test4Stat]=ValvePWM(2,5,29000,10,500,460,500);
+[Test5Data,Test5Stat]=ValvePWM(2,5,29000,10,500,440,500);
+[Test6Data,Test6Stat]=ValvePWM(2,5,29000,10,500,440,500);
+[Test7Data,Test7Stat]=ValvePWM(2,5,29000,5,500,460,500);
+[Test8Data,Test8Stat]=ValvePWM(2,5,29000,5,500,460,500);
+[Test9Data,Test9Stat]=ValvePWM(2,5,29000,5,500,450,500);
+[Test10Data,Test10Stat]=ValvePWM(2,5,29000,5,500,450,500);

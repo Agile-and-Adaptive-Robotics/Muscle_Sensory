36467385cf6f8377a04d445ddc019b4c77b23246
Lawrence Pang <64503730+pnut9@users.noreply.github.com>
2022-03-11 17:01:18 -0800

    Correcting 20mm Data

--- DataCollection/AllData_raw_csv/Correcting20mmData.m
+++ DataCollection/AllData_raw_csv/Correcting20mmData.m
@@ -3,7 +3,7 @@
 %data and write over current data. 
 
 
-%% 20mm 10cm (12.4cm): Unkink, 13mm, 20mm
+%% 20mm 10cm (12.4cm): Unkink, 13mm, 20mm (Pressure and Force Column = A0 Raw)
 Kinks_20mm10cm = {'10cm_Unkinked_Test','10cm_Kinked13mm_Test','10cm_Kinked20mm_Test'};
 
 for a = 1:length(Kinks_20mm10cm)
@@ -12,12 +12,17 @@ for a = 1:length(Kinks_20mm10cm)
         i
         test=num2str(i);
         k = sprintf('20mm_%s%sData.csv',Kinks_20mm10cm{a},test)
-        Data = csvread(k);
+        Data = csvread(k)
+        Force = (((Data(:,1))*0.392) -4.1786)*4.45;
+        Pressure = (Data(:,2)*0.7654)-18.609
+        Data(:,1) = Force;
+        Data(:,2) = Pressure;
+
     end
 end
 
 
-%% 20mm 12cm (15cm) : Unkinked, 10mm, 20mm
+%% 20mm 12cm (15cm) : Unkinked, 10mm, 20mm (Force output = A0 raw, Pressure = kPA)
 Kinks_20mm12cm = {'12cm_Unkinked_Test','12cm_Kinked10mm_Test','12cm_Kinked20mm_Test'};
 
 for a = 1:length(Kinks_20mm12cm)
@@ -27,6 +32,9 @@ for a = 1:length(Kinks_20mm12cm)
         test=num2str(i);
         k = sprintf('20mm_%s%s.csv',Kinks_20mm12cm{a},test)
         Data=csvread(k);
+        Force = ((Data(:,1)*0.392) - 4.1786)*4.45;
+        Data(:,1) = Force;
+
     end
 end
 
@@ -40,6 +48,8 @@ for a = 1:length(Kinks_20mm23cm)
         test=num2str(i);
         k = sprintf('20mm_%s%s.csv',Kinks_20mm23cm{a},test)
         Data=csvread(k);
+        Force = ((Data(:,1)*0.392) - 4.1786)*4.45;
+        Data(:,1) = Force;
     end
 end
 
@@ -54,11 +64,13 @@ for a = 1:length(Kinks_20mm30cm)
         test=num2str(i);
         k = sprintf('20mm_%s%s.csv',Kinks_20mm30cm{a},test)
         Data=csvread(k);
+        Force = ((Data(:,1)*0.392) - 4.1786)*4.45;
+        Data(:,1) = Force;
     end
 end
 
 
-%% 20mm 40cm (42.7cm): Unkinked, 35mm, 46mm, 69mm
+%% 20mm 40cm (42.7cm): Unkinked, 35mm, 46mm, 69mm(Pressure and Force Column = A0 Raw)
 Kinks_20mm40cm ={'40cm_Unkinked_Test','40cm_Kinked35mm_Test','40cm_Kinked46mm_Test','40cm_Kinked69mm_Test'};
 
 for a = 1:length(Kinks_20mm40cm)
@@ -68,5 +80,9 @@ for a = 1:length(Kinks_20mm40cm)
         test=num2str(i);
         k = sprintf('20mm_%s%sData.csv',Kinks_20mm40cm{a},test)
         Data=csvread(k);
+        Force = (((Data(:,1))*0.392) -4.1786)*4.45;
+        Pressure = (Data(:,2)*0.7654)-18.609
+        Data(:,1) = Force;
+        Data(:,2) = Pressure;
     end
 end

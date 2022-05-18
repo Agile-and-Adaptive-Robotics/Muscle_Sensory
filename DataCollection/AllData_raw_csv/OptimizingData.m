a9c4c84f506508f01abf46f29308dae2967ac6e7
Lawrence Pang <lawrence_91096@hotmail.com>
2022-05-18 16:21:02 -0700

    Assigning States for Pressurizing and Depressurizing

--- DataCollection/AllData_raw_csv/OptimizingData.m
+++ DataCollection/AllData_raw_csv/OptimizingData.m
@@ -6,10 +6,10 @@
 %Eg: model equation:  y = Aexp(-lamda*t); A & lamda are parameters to be
 %tuned. To fit curve, we need to minimize sum of squared error
 testnumber = 9;
-li= 12;
-lo= 10; 
+li= 10.8;
+lo= 12; 
 kinks = [0,4,8,12] 
-data= AllBPA10mm13cm(AllBPA10mm13cm(:,5)==13& AllBPA10mm13cm(:,6)==4&AllBPA10mm13cm(:,7)==testnumber,:); %Data for optimization (F,P,t,d,l,kink,test#)
+data= AllBPA10mm13cm(AllBPA10mm13cm(:,5)==13& AllBPA10mm13cm(:,6)==12&AllBPA10mm13cm(:,7)==testnumber,:); %Data for optimization (F,P,t,d,l,kink,test#)
 data_pressure = data(:,2)
 data_force = data(:,1)
 
@@ -21,22 +21,25 @@ xlabel('Pressure (kPa)')
 ylabel('Force (N)')
 
 %% using fmin search to optimize
-fun = @(x)sseval(x,data_pressure,data_force);
+fun = @(x)sseval(x,data_pressure,data_force,li,lo);
 
 %Finding best fitting parameters
-x0 = [0.6184,-41.219] %random values 
+x0 = [0.6184,-41.219,2.1,3] %random values 
 bestx = fminsearch(fun,x0) %finding the
 
 %Check fit quality
 a0 = bestx(1); 
 a1 = bestx(2);
-%a2 = bestx(3);
+a2 = bestx(3);
+a3 = bestx(4);
 
 %EQ1: 
-yfit = a0*data_pressure+a1;
+yfit = (a0 + a1*(li-lo)/lo)*data_pressure + a2*(li-lo)/lo +a3;
+%a0*data_pressure+a1;
 %EQ2: yfit = (a0+a1*(li -lo))*data_pressure;
 plot(data_pressure,data_force,'*');
 hold on
+
 plot(data_pressure,yfit,'r')
 xlabel('Pressure(kPa)')
 ylabel('Force data and Fit')
--- DataCollection/AllData_raw_csv/sseval.m
+++ DataCollection/AllData_raw_csv/sseval.m
@@ -1,6 +1,9 @@
 %Compute sum of square error
 
-function sse = sseval(x,tdata,ydata)
+function sse = sseval(x,tdata,ydata,li,lo) %tdata = pressure
 a0 = x(1);
 a1 =x(2);
-sse = sum((ydata-(a0*tdata+a1)).^2);
+a2 = x(3);
+a3 = x(4);
+model = (a0 + a1*(li-lo)/lo)*tdata + a2*(li-lo)/lo +a3 ;
+sse =sum((ydata-model).^2) %sum((ydata-(a0*tdata+a1)).^2);

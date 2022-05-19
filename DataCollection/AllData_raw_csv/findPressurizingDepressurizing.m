0187601514bdc2722f91cf1489a21882362e6db0
Lawrence Pang <lawrence_91096@hotmail.com>
2022-05-18 17:03:29 -0700

    update

--- DataCollection/AllData_raw_csv/findPressurizingDepressurizing.m
+++ DataCollection/AllData_raw_csv/findPressurizingDepressurizing.m
@@ -1,6 +1,14 @@
 idx_P = (AllBPA10mm13cm(:,8) ==1)
-
+figure
 plot(AllBPA10mm13cm(idx_P,3),AllBPA10mm13cm(idx_P,1),'o')
-
+figure
 idx_DP = AllBPA10mm13cm(:,8) ==0
 plot(AllBPA10mm13cm(idx_DP,3),AllBPA10mm13cm(idx_DP,1),'.')
+
+%% 
+idx_P = (AllBPA10mm23cm(:,8) ==1)
+figure
+plot(AllBPA10mm23cm(idx_P,3),AllBPA10mm23cm(idx_P,1),'o')
+figure
+idx_DP = AllBPA10mm23cm(:,8) ==0
+plot(AllBPA10mm23cm(idx_DP,3),AllBPA10mm23cm(idx_DP,1),'.')

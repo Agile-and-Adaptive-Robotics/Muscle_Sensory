3073af5a46ebd36ed916bcb11b79c18c9a89abd4
Lawrence Pang <lawrence_91096@hotmail.com>
2022-07-18 17:00:59 -0700

    Update OptimizingData_.m

--- DataCollection/AllData_raw_csv/OptimizingData_.m
+++ DataCollection/AllData_raw_csv/OptimizingData_.m
@@ -43,3 +43,17 @@
  
  
 % COMMENT: need to separate by test number too. 
+
+%% using fmin search to optimize data
+fun = @(x)sseval(x,data_pressure,data_force);
+
+%Best fitting parameters(x0)
+x0 = [0.6184,-41.219,2.1,3]; %random values (#of parameters = # of entries) 
+bestx =fminsearch(fun,x0) 
+
+
+%Test fit quality
+a0 = bestx(1);
+a1 = bestx(2);
+a2 = bestx(3);
+a3 = bestx(4);

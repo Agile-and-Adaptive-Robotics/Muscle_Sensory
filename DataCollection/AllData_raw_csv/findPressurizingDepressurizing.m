a63145ac61600ff573c7fbe4e66a469f53f2be9e
Lawrence Pang <64503730+pnut9@users.noreply.github.com>
2022-09-14 12:27:16 -0700

    Revert "?"
    This reverts commit ba956a58db6c06af00cd21b725bdbe5ae0ac15a8.

--- DataCollection/AllData_raw_csv/findPressurizingDepressurizing.m
+++ DataCollection/AllData_raw_csv/findPressurizingDepressurizing.m
@@ -1,7 +1,6 @@
 idx_P = (AllBPA10mm13cm(:,8) ==1)
-figure
+
 plot(AllBPA10mm13cm(idx_P,3),AllBPA10mm13cm(idx_P,1),'o')
 
-idx_DP = AllBPA10mm13cm(:,8) ==0  
-figure
+idx_DP = AllBPA10mm13cm(:,8) ==0
 plot(AllBPA10mm13cm(idx_DP,3),AllBPA10mm13cm(idx_DP,1),'.')

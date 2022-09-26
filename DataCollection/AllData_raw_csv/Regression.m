f7043d65840bb99b289f1128ef5bcde0f0705c73
Lawrence Pang <lawrence_91096@hotmail.com>
2022-09-26 16:49:42 -0700

    Model 7 - Strain

--- DataCollection/AllData_raw_csv/Regression.m
+++ DataCollection/AllData_raw_csv/Regression.m
@@ -1,25 +1,28 @@
 % regression to optimize equation
 
 %k = readmatrix('OptimizingParameters.xlsx','Sheet','Model7_2')
-diameter=10; lengths=13; test=10;
+diameter=20; lengths=40; test=10;
 [diameter,lengths,lo,li,l620,kink,kmax,testnum,State,B0,B1] = choosedata(diameter,lengths,test);
 strain = (lo - li)./lo;
 max_strain = (lo - l620)./ lo;
 relative_strain = strain./max_strain;
 
 %%  creating regression matrix
-x1 = (1-relative_strain)';
-y = B1(1,:)'; 
-X = [ones(size(y)) x1];
-b = regress(y,X);
+for i = 1:2 %cycles P and DP. 
+    x1 = (1-relative_strain)';
+    y = B0(i,:)'; 
+    X = [ones(size(y)) x1];
+    c = regress(y,X);
+    c
 
 
-% plot fit to compare
-figure
-yfit = b(1) + x1*b(2);
-plot(x1,yfit);
-hold on
-plot(x1,y,'o')
-hold off
-xlabel('1-relative strain')
-ylabel('B1 - Slope')
+    % plot fit to compare
+    figure(i)
+    yfit = c(1) + x1*c(2);
+    plot(x1,yfit);
+    hold on
+    plot(x1,y,'o')
+    hold off
+    xlabel('1-relative strain')
+    ylabel('B1 - Slope')
+end

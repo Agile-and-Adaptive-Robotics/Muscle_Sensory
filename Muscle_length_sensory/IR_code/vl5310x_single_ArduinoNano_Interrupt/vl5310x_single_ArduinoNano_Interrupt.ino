ec38c8fbe2e242ec3c57d1a5d84af2e608cf2f4f
Jake Chung <luonchung@gmail.com>
2022-11-27 09:37:08 -0800

    collecting sensor length data with the built-in interrupt function

---
+++ Muscle_length_sensory/IR_code/vl5310x_single_ArduinoNano_Interrupt/vl5310x_single_ArduinoNano_Interrupt.ino
@@ -0,0 +1,39 @@
+/* This example reads the sensor output at a fixed interval 
+controlled sensor API
+
+The range readings are in units of mm. */
+
+#include <Wire.h>
+#include <VL53L0X.h>
+
+VL53L0X sensor;
+
+void setup()
+{
+  Serial.begin(9600);
+  Wire.begin();
+
+  pinMode(12, OUTPUT);  // timing pin to check sampling frequency
+
+  sensor.setTimeout(500);
+  if (!sensor.init())
+  {
+    Serial.println("Failed to detect and initialize sensor!");
+    while (1) {}
+  }
+
+  // Start continuous back-to-back mode (take readings as
+  // fast as possible).  To use continuous timed mode
+  // instead, provide a desired inter-measurement period in
+  // ms (e.g. sensor.startContinuous(100)).
+  sensor.startContinuous(100);
+}
+
+void loop()
+{
+  Serial.print(sensor.readRangeContinuousMillimeters());
+  digitalWrite(12, !digitalRead(12));  // toggle the pin to check frequency
+  if (sensor.timeoutOccurred()) { Serial.print(" TIMEOUT"); }
+
+  Serial.println();
+}

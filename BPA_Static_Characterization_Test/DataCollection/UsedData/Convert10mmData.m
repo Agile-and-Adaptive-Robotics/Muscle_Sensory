2651db2a8aa154b0ee2c8e506ca4c495a368ebfe
bbolen83 <59748829+bbolen83@users.noreply.github.com>
2023-12-13 23:03:00 -0800

    ConvertCSVtoMat works
    use it in the raw data folder. Saved data in allData_Ben.mat

--- BPA_Static_Characterization_Test/DataCollection/UsedData/Convert10mmData.m
+++ BPA_Static_Characterization_Test/DataCollection/UsedData/Convert10mmData.m
@@ -1,68 +1,68 @@
-% This script reads csv file and convert data into proper Pressure(kPa)and
-% Force(N). 10mm Data. 
-Data_10mm_13cm_Unkinked_Test1 = csvread('10mm_13cm_Unkinked_Test1.csv');
-Data_10mm_13cm_Unkinked_Test1([1:150],:)=[];
-    Force_10mm_13cm_Unkinked_Test1_Wrong = Data_10mm_13cm_Unkinked_Test1(:,1);
-    Force_10mm_13cm_Unkinked_Test1_A0=((Force_10mm_13cm_Unkinked_Test1_Wrong/4.45) +30.882)/1.6475; %convert back to Arduino Output
-    Force_10mm_13cm_Unkinked_Test1_Offset = Force_10mm_13cm_Unkinked_Test1_A0 - min(Force_10mm_13cm_Unkinked_Test1_A0);
-    Force_10mm_13cm_Unkinked_Test1 = (((Force_10mm_13cm_Unkinked_Test1_Offset)*0.1535)-1.963)*4.45; %corrected Force output (N)
-    Pressure_10mm_13cm_Unkinked_Test1 =Data_10mm_13cm_Unkinked_Test1(:,2);
-    Time_10mm_13cm_Unkinked_Test1 = Data_10mm_13cm_Unkinked_Test1(:,3);
-Data_10mm_13cm_Unkinked_Test2 = csvread('10mm_13cm_Unkinked_Test2.csv');
-    Force_10mm_13cm_Unkinked_Test2_Wrong = Data_10mm_13cm_Unkinked_Test2(:,1);
-    Force_10mm_13cm_Unkinked_Test2_A0=((Force_10mm_13cm_Unkinked_Test2_Wrong/4.45) +30.882)/1.6475; %convert back to Arduino Output
-    Force_10mm_13cm_Unkinked_Test2_Offset = Force_10mm_13cm_Unkinked_Test2_A0 - min(Force_10mm_13cm_Unkinked_Test2_A0);
-    Force_10mm_13cm_Unkinked_Test2 = (((Force_10mm_13cm_Unkinked_Test2_Offset)*0.1535)-1.963)*4.45; %corrected Force output (N)
-    Pressure_10mm_13cm_Unkinked_Test1 =Data_10mm_13cm_Unkinked_Test2(:,2);
-    Time_10mm_13cm_Unkinked_Test2 = Data_10mm_13cm_Unkinked_Test2(:,3);
-Data_10mm_13cm_Unkinked_Test3 = csvread('10mm_13cm_Unkinked_Test3.csv');
-    Force_10mm_13cm_Unkinked_Test3_Wrong = Data_10mm_13cm_Unkinked_Test3(:,1);
-    Force_10mm_13cm_Unkinked_Test3_A0=((Force_10mm_13cm_Unkinked_Test3_Wrong/4.45) +30.882)/1.6475; %convert back to Arduino Output
-    Force_10mm_13cm_Unkinked_Test3_Offset = Force_10mm_13cm_Unkinked_Test3_A0 - min(Force_10mm_13cm_Unkinked_Test3_A0);
-    Force_10mm_13cm_Unkinked_Test3 = (((Force_10mm_13cm_Unkinked_Test3_Offset)*0.1535)-1.963)*4.45; %corrected Force output (N)
-    Pressure_10mm_13cm_Unkinked_Test3 =Data_10mm_13cm_Unkinked_Test3(:,2);
-    Time_10mm_13cm_Unkinked_Test3 = Data_10mm_13cm_Unkinked_Test3(:,3)
-    Force_10mm_13cm_Unkinked_Test3 = (((Force_10mm_13cm_Unkinked_Test3_Offset)*0.1535)-1.963)*4.45; %corrected Force output (N)
-Data_10mm_13cm_Unkinked_Test4 = csvread('10mm_13cm_Unkinked_Test4.csv');
-    Force_10mm_13cm_Unkinked_Test4_Wrong = Data_10mm_13cm_Unkinked_Test4(:,1);
-    Force_10mm_13cm_Unkinked_Test4_A0=((Force_10mm_13cm_Unkinked_Test4_Wrong/4.45) +30.882)/1.6475; %convert back to Arduino Output
-    Force_10mm_13cm_Unkinked_Test4_Offset = Force_10mm_13cm_Unkinked_Test4_A0 - min(Force_10mm_13cm_Unkinked_Test3_A0);
-    Force_10mm_13cm_Unkinked_Test4 = (((Force_10mm_13cm_Unkinked_Test4_Offset)*0.1535)-1.963)*4.45; %corrected Force output (N)
-    Pressure_10mm_13cm_Unkinked_Test4 =Data_10mm_13cm_Unkinked_Test4(:,2);
-    Time_10mm_13cm_Unkinked_Test4 = Data_10mm_13cm_Unkinked_Test4(:,3)
-    Force_10mm_13cm_Unkinked_Test4 = (((Force_10mm_13cm_Unkinked_Test4_Offset)*0.1535)-1.963)*4.45; %corrected Force output (N)
-Data_10mm_13cm_Unkinked_Test5 = csvread('10mm_13cm_Unkinked_Test5.csv');
-    Force_10mm_13cm_Unkinked_Test5_Wrong = Data_10mm_13cm_Unkinked_Test5(:,1);
-    Force_10mm_13cm_Unkinked_Test5_A0=((Force_10mm_13cm_Unkinked_Test5_Wrong/4.45) +30.882)/1.6475; %convert back to Arduino Output
-    Force_10mm_13cm_Unkinked_Test5_Offset = Force_10mm_13cm_Unkinked_Test5_A0 - min(Force_10mm_13cm_Unkinked_Test5_A0);
-    Force_10mm_13cm_Unkinked_Test5 = (((Force_10mm_13cm_Unkinked_Test5_Offset)*0.1535)-1.963)*4.45; %corrected Force output (N)
-    Pressure_10mm_13cm_Unkinked_Test5 =Data_10mm_13cm_Unkinked_Test5(:,2);
-    Time_10mm_13cm_Unkinked_Test5 = Data_10mm_13cm_Unkinked_Test5(:,3)
-    Force_10mm_13cm_Unkinked_Test5 = (((Force_10mm_13cm_Unkinked_Test5_Offset)*0.1535)-1.963)*4.45; %corrected Force output (N)
-Data_10mm_13cm_Unkinked_Test6 = csvread('10mm_13cm_Unkinked_Test6.csv');
-    Force_10mm_13cm_Unkinked_Test6_Wrong = Data_10mm_13cm_Unkinked_Test6(:,1);
-    Force_10mm_13cm_Unkinked_Test6_A0=((Force_10mm_13cm_Unkinked_Test6_Wrong/4.45) +30.882)/1.6475; %convert back to Arduino Output
-    Force_10mm_13cm_Unkinked_Test6_Offset = Force_10mm_13cm_Unkinked_Test6_A0 - min(Force_10mm_13cm_Unkinked_Test6_A0);
-    Force_10mm_13cm_Unkinked_Test6 = (((Force_10mm_13cm_Unkinked_Test6_Offset)*0.1535)-1.963)*4.45; %corrected Force output (N)
-    Pressure_10mm_13cm_Unkinked_Test6 =Data_10mm_13cm_Unkinked_Test6(:,2);
-    Time_10mm_13cm_Unkinked_Test6 = Data_10mm_13cm_Unkinked_Test6(:,3)
-    Force_10mm_13cm_Unkinked_Test6 = (((Force_10mm_13cm_Unkinked_Test6_Offset)*0.1535)-1.963)*4.45; %corrected Force output (N)
-Data_10mm_13cm_Unkinked_Test7 = csvread('10mm_13cm_Unkinked_Test7.csv');
-    Force_10mm_13cm_Unkinked_Test7_Wrong = Data_10mm_13cm_Unkinked_Test7(:,1);
-    Force_10mm_13cm_Unkinked_Test7_A0=((Force_10mm_13cm_Unkinked_Test7_Wrong/4.45) +30.882)/1.6475; %convert back to Arduino Output
-    Force_10mm_13cm_Unkinked_Test7_Offset = Force_10mm_13cm_Unkinked_Test7_A0 - min(Force_10mm_13cm_Unkinked_Test7_A0);
-    Force_10mm_13cm_Unkinked_Test7 = (((Force_10mm_13cm_Unkinked_Test7_Offset)*0.1535)-1.963)*4.45; %corrected Force output (N)
-    Pressure_10mm_13cm_Unkinked_Test7 =Data_10mm_13cm_Unkinked_Test7(:,2);
-    Time_10mm_13cm_Unkinked_Test7 = Data_10mm_13cm_Unkinked_Test7(:,3)
-    Force_10mm_13cm_Unkinked_Test7 = (((Force_10mm_13cm_Unkinked_Test7_Offset)*0.1535)-1.963)*4.45; %corrected Force output (N)
-Data_10mm_13cm_Unkinked_Test8 = csvread('10mm_13cm_Unkinked_Test8.csv');
-    Force_10mm_13cm_Unkinked_Test8_Wrong = Data_10mm_13cm_Unkinked_Test8(:,1);
-    Force_10mm_13cm_Unkinked_Test8_A0=((Force_10mm_13cm_Unkinked_Test8_Wrong/4.45) +30.882)/1.6475; %convert back to Arduino Output
-    Force_10mm_13cm_Unkinked_Test8_Offset = Force_10mm_13cm_Unkinked_Test8_A0 - min(Force_10mm_13cm_Unkinked_Test8_A0);
-    Force_10mm_13cm_Unkinked_Test8 = (((Force_10mm_13cm_Unkinked_Test8_Offset)*0.1535)-1.963)*4.45; %corrected Force output (N)
-    Pressure_10mm_13cm_Unkinked_Test8 =Data_10mm_13cm_Unkinked_Test8(:,2);
-    Time_10mm_13cm_Unkinked_Test8 = Data_10mm_13cm_Unkinked_Test8(:,3)
-    Force_10mm_13cm_Unkinked_Test8 = (((Force_10mm_13cm_Unkinked_Test8_Offset)*0.1535)-1.963)*4.45; %corrected Force output (N)
+% % This script reads csv file and convert data into proper Pressure(kPa)and
+% % Force(N). 10mm Data. 
+% Data_10mm_13cm_Unkinked_Test1 = csvread('10mm_13cm_Unkinked_Test1.csv');
+% Data_10mm_13cm_Unkinked_Test1([1:150],:)=[];
+%     Force_10mm_13cm_Unkinked_Test1_Wrong = Data_10mm_13cm_Unkinked_Test1(:,1);
+%     Force_10mm_13cm_Unkinked_Test1_A0=((Force_10mm_13cm_Unkinked_Test1_Wrong/4.45) +30.882)/1.6475; %convert back to Arduino Output
+%     Force_10mm_13cm_Unkinked_Test1_Offset = Force_10mm_13cm_Unkinked_Test1_A0 - min(Force_10mm_13cm_Unkinked_Test1_A0);
+%     Force_10mm_13cm_Unkinked_Test1 = (((Force_10mm_13cm_Unkinked_Test1_Offset)*0.1535)-1.963)*4.45; %corrected Force output (N)
+%     Pressure_10mm_13cm_Unkinked_Test1 =Data_10mm_13cm_Unkinked_Test1(:,2);
+%     Time_10mm_13cm_Unkinked_Test1 = Data_10mm_13cm_Unkinked_Test1(:,3);
+% Data_10mm_13cm_Unkinked_Test2 = csvread('10mm_13cm_Unkinked_Test2.csv');
+%     Force_10mm_13cm_Unkinked_Test2_Wrong = Data_10mm_13cm_Unkinked_Test2(:,1);
+%     Force_10mm_13cm_Unkinked_Test2_A0=((Force_10mm_13cm_Unkinked_Test2_Wrong/4.45) +30.882)/1.6475; %convert back to Arduino Output
+%     Force_10mm_13cm_Unkinked_Test2_Offset = Force_10mm_13cm_Unkinked_Test2_A0 - min(Force_10mm_13cm_Unkinked_Test2_A0);
+%     Force_10mm_13cm_Unkinked_Test2 = (((Force_10mm_13cm_Unkinked_Test2_Offset)*0.1535)-1.963)*4.45; %corrected Force output (N)
+%     Pressure_10mm_13cm_Unkinked_Test1 =Data_10mm_13cm_Unkinked_Test2(:,2);
+%     Time_10mm_13cm_Unkinked_Test2 = Data_10mm_13cm_Unkinked_Test2(:,3);
+% Data_10mm_13cm_Unkinked_Test3 = csvread('10mm_13cm_Unkinked_Test3.csv');
+%     Force_10mm_13cm_Unkinked_Test3_Wrong = Data_10mm_13cm_Unkinked_Test3(:,1);
+%     Force_10mm_13cm_Unkinked_Test3_A0=((Force_10mm_13cm_Unkinked_Test3_Wrong/4.45) +30.882)/1.6475; %convert back to Arduino Output
+%     Force_10mm_13cm_Unkinked_Test3_Offset = Force_10mm_13cm_Unkinked_Test3_A0 - min(Force_10mm_13cm_Unkinked_Test3_A0);
+%     Force_10mm_13cm_Unkinked_Test3 = (((Force_10mm_13cm_Unkinked_Test3_Offset)*0.1535)-1.963)*4.45; %corrected Force output (N)
+%     Pressure_10mm_13cm_Unkinked_Test3 =Data_10mm_13cm_Unkinked_Test3(:,2);
+%     Time_10mm_13cm_Unkinked_Test3 = Data_10mm_13cm_Unkinked_Test3(:,3)
+%     Force_10mm_13cm_Unkinked_Test3 = (((Force_10mm_13cm_Unkinked_Test3_Offset)*0.1535)-1.963)*4.45; %corrected Force output (N)
+% Data_10mm_13cm_Unkinked_Test4 = csvread('10mm_13cm_Unkinked_Test4.csv');
+%     Force_10mm_13cm_Unkinked_Test4_Wrong = Data_10mm_13cm_Unkinked_Test4(:,1);
+%     Force_10mm_13cm_Unkinked_Test4_A0=((Force_10mm_13cm_Unkinked_Test4_Wrong/4.45) +30.882)/1.6475; %convert back to Arduino Output
+%     Force_10mm_13cm_Unkinked_Test4_Offset = Force_10mm_13cm_Unkinked_Test4_A0 - min(Force_10mm_13cm_Unkinked_Test3_A0);
+%     Force_10mm_13cm_Unkinked_Test4 = (((Force_10mm_13cm_Unkinked_Test4_Offset)*0.1535)-1.963)*4.45; %corrected Force output (N)
+%     Pressure_10mm_13cm_Unkinked_Test4 =Data_10mm_13cm_Unkinked_Test4(:,2);
+%     Time_10mm_13cm_Unkinked_Test4 = Data_10mm_13cm_Unkinked_Test4(:,3)
+%     Force_10mm_13cm_Unkinked_Test4 = (((Force_10mm_13cm_Unkinked_Test4_Offset)*0.1535)-1.963)*4.45; %corrected Force output (N)
+% Data_10mm_13cm_Unkinked_Test5 = csvread('10mm_13cm_Unkinked_Test5.csv');
+%     Force_10mm_13cm_Unkinked_Test5_Wrong = Data_10mm_13cm_Unkinked_Test5(:,1);
+%     Force_10mm_13cm_Unkinked_Test5_A0=((Force_10mm_13cm_Unkinked_Test5_Wrong/4.45) +30.882)/1.6475; %convert back to Arduino Output
+%     Force_10mm_13cm_Unkinked_Test5_Offset = Force_10mm_13cm_Unkinked_Test5_A0 - min(Force_10mm_13cm_Unkinked_Test5_A0);
+%     Force_10mm_13cm_Unkinked_Test5 = (((Force_10mm_13cm_Unkinked_Test5_Offset)*0.1535)-1.963)*4.45; %corrected Force output (N)
+%     Pressure_10mm_13cm_Unkinked_Test5 =Data_10mm_13cm_Unkinked_Test5(:,2);
+%     Time_10mm_13cm_Unkinked_Test5 = Data_10mm_13cm_Unkinked_Test5(:,3)
+%     Force_10mm_13cm_Unkinked_Test5 = (((Force_10mm_13cm_Unkinked_Test5_Offset)*0.1535)-1.963)*4.45; %corrected Force output (N)
+% Data_10mm_13cm_Unkinked_Test6 = csvread('10mm_13cm_Unkinked_Test6.csv');
+%     Force_10mm_13cm_Unkinked_Test6_Wrong = Data_10mm_13cm_Unkinked_Test6(:,1);
+%     Force_10mm_13cm_Unkinked_Test6_A0=((Force_10mm_13cm_Unkinked_Test6_Wrong/4.45) +30.882)/1.6475; %convert back to Arduino Output
+%     Force_10mm_13cm_Unkinked_Test6_Offset = Force_10mm_13cm_Unkinked_Test6_A0 - min(Force_10mm_13cm_Unkinked_Test6_A0);
+%     Force_10mm_13cm_Unkinked_Test6 = (((Force_10mm_13cm_Unkinked_Test6_Offset)*0.1535)-1.963)*4.45; %corrected Force output (N)
+%     Pressure_10mm_13cm_Unkinked_Test6 =Data_10mm_13cm_Unkinked_Test6(:,2);
+%     Time_10mm_13cm_Unkinked_Test6 = Data_10mm_13cm_Unkinked_Test6(:,3)
+%     Force_10mm_13cm_Unkinked_Test6 = (((Force_10mm_13cm_Unkinked_Test6_Offset)*0.1535)-1.963)*4.45; %corrected Force output (N)
+% Data_10mm_13cm_Unkinked_Test7 = csvread('10mm_13cm_Unkinked_Test7.csv');
+%     Force_10mm_13cm_Unkinked_Test7_Wrong = Data_10mm_13cm_Unkinked_Test7(:,1);
+%     Force_10mm_13cm_Unkinked_Test7_A0=((Force_10mm_13cm_Unkinked_Test7_Wrong/4.45) +30.882)/1.6475; %convert back to Arduino Output
+%     Force_10mm_13cm_Unkinked_Test7_Offset = Force_10mm_13cm_Unkinked_Test7_A0 - min(Force_10mm_13cm_Unkinked_Test7_A0);
+%     Force_10mm_13cm_Unkinked_Test7 = (((Force_10mm_13cm_Unkinked_Test7_Offset)*0.1535)-1.963)*4.45; %corrected Force output (N)
+%     Pressure_10mm_13cm_Unkinked_Test7 =Data_10mm_13cm_Unkinked_Test7(:,2);
+%     Time_10mm_13cm_Unkinked_Test7 = Data_10mm_13cm_Unkinked_Test7(:,3)
+%     Force_10mm_13cm_Unkinked_Test7 = (((Force_10mm_13cm_Unkinked_Test7_Offset)*0.1535)-1.963)*4.45; %corrected Force output (N)
+% Data_10mm_13cm_Unkinked_Test8 = csvread('10mm_13cm_Unkinked_Test8.csv');
+%     Force_10mm_13cm_Unkinked_Test8_Wrong = Data_10mm_13cm_Unkinked_Test8(:,1);
+%     Force_10mm_13cm_Unkinked_Test8_A0=((Force_10mm_13cm_Unkinked_Test8_Wrong/4.45) +30.882)/1.6475; %convert back to Arduino Output
+%     Force_10mm_13cm_Unkinked_Test8_Offset = Force_10mm_13cm_Unkinked_Test8_A0 - min(Force_10mm_13cm_Unkinked_Test8_A0);
+%     Force_10mm_13cm_Unkinked_Test8 = (((Force_10mm_13cm_Unkinked_Test8_Offset)*0.1535)-1.963)*4.45; %corrected Force output (N)
+%     Pressure_10mm_13cm_Unkinked_Test8 =Data_10mm_13cm_Unkinked_Test8(:,2);
+%     Time_10mm_13cm_Unkinked_Test8 = Data_10mm_13cm_Unkinked_Test8(:,3)
+%     Force_10mm_13cm_Unkinked_Test8 = (((Force_10mm_13cm_Unkinked_Test8_Offset)*0.1535)-1.963)*4.45; %corrected Force output (N)
 Data_10mm_13cm_Unkinked_Test9 = csvread('10mm_13cm_Unkinked_Test9.csv');
     Force_10mm_13cm_Unkinked_Test9_Wrong = Data_10mm_13cm_Unkinked_Test9(:,1);
     Force_10mm_13cm_Unkinked_Test9_A0=((Force_10mm_13cm_Unkinked_Test9_Wrong/4.45) +30.882)/1.6475; %convert back to Arduino Output
@@ -81,14 +81,14 @@ Data_10mm_13cm_Unkinked_Test10 = csvread('10mm_13cm_Unkinked_Test10.csv');
     Force_10mm_13cm_Unkinked_Test10 = (((Force_10mm_13cm_Unkinked_Test10_Offset)*0.1535)-1.963)*4.45; %corrected Force output (N)
 
 figure
-plot(Time_10mm_13cm_Unkinked_Test1,Force_10mm_13cm_Unkinked_Test1)
-plot(Time_10mm_13cm_Unkinked_Test2,Force_10mm_13cm_Unkinked_Test2)
-plot(Time_10mm_13cm_Unkinked_Test3,Force_10mm_13cm_Unkinked_Test3)
-plot(Time_10mm_13cm_Unkinked_Test4,Force_10mm_13cm_Unkinked_Test4)
-plot(Time_10mm_13cm_Unkinked_Test5,Force_10mm_13cm_Unkinked_Test5)
-plot(Time_10mm_13cm_Unkinked_Test6,Force_10mm_13cm_Unkinked_Test6)
-plot(Time_10mm_13cm_Unkinked_Test7,Force_10mm_13cm_Unkinked_Test7)
-plot(Time_10mm_13cm_Unkinked_Test8,Force_10mm_13cm_Unkinked_Test8)
+% plot(Time_10mm_13cm_Unkinked_Test1,Force_10mm_13cm_Unkinked_Test1)
+% plot(Time_10mm_13cm_Unkinked_Test2,Force_10mm_13cm_Unkinked_Test2)
+% plot(Time_10mm_13cm_Unkinked_Test3,Force_10mm_13cm_Unkinked_Test3)
+% plot(Time_10mm_13cm_Unkinked_Test4,Force_10mm_13cm_Unkinked_Test4)
+% plot(Time_10mm_13cm_Unkinked_Test5,Force_10mm_13cm_Unkinked_Test5)
+% plot(Time_10mm_13cm_Unkinked_Test6,Force_10mm_13cm_Unkinked_Test6)
+% plot(Time_10mm_13cm_Unkinked_Test7,Force_10mm_13cm_Unkinked_Test7)
+% plot(Time_10mm_13cm_Unkinked_Test8,Force_10mm_13cm_Unkinked_Test8)
 plot(Time_10mm_13cm_Unkinked_Test9,Force_10mm_13cm_Unkinked_Test9)
 plot(Time_10mm_13cm_Unkinked_Test10,Force_10mm_13cm_Unkinked_Test10)
 
--- BPA_Static_Characterization_Test/DataCollection/UsedData/ConvertCSVtoMAT.m
+++ BPA_Static_Characterization_Test/DataCollection/UsedData/ConvertCSVtoMAT.m
@@ -24,7 +24,7 @@ l620(1) = 10;
 vals{1}= [0,4,8,12];
 
 %23 cm
-Kinks{2,:} = {'27cm_Unkinked_Test', '27cm_Kinked7mm_Test','27cm_Kinked15mm_Test','27cm_Kinked31mm_Test'};
+Kinks{2,:} = {'23cm_Unkinked_Test', '23cm_Kinked14mm_Test','23cm_Kinked30mm_Test'};
 vals{2}= [0,14,30];
 l620(2) = 18.5;
 li{2} = [22,20.5,18.9];
@@ -57,20 +57,20 @@ for i = 1:length(Cut)
 end
 
 
-test = ["9", "10"];
+test = ["8", "9", "10"];
 
 [a,b] = size(relE);
 c = length(test);
 T = cell(a,b,c);
 for k = 1:length(test)
-    for j = 1:length(Cut)
-        for i = 1:length(li{j})
-         str ="10mm_"+(string(Kinks{j}{1,i})+test(k)+".csv");   
+    for i = 1:length(Cut)
+        for j = 1:length(li{i})
+         str ="10mm_"+(string(Kinks{i}{1,j})+test(k)+".csv");   
          M = readmatrix(str);
-         M = M((M(:,3)<=17 &M(:,1)>low_force &M(:,2)<high_pressure),:);
-         M = imresize(M,[num 3]);
-         M = [ones(length(M),1).*relE{j,i}(:), M(:,2), M(:,1), M(:,3)];
-         T{j,i,k} = M(:,:);
+         M = M((M(:,3)<=17 &M(:,1)>low_force &M(:,2)<high_pressure),:);  %keep values that are pressurizing (less than 17second, high than low force, lower than high pressure
+         M = imresize(M,[num 3],'nearest');
+         M = [ones(length(M),1).*relE{i,j}(:), M(:,2), M(:,1), M(:,3)];
+         T{i,j,k} = M(:,:);
         end
     end
 end
@@ -79,61 +79,31 @@ end
 %% plots to check data
 
 
-
+varS = ["Relative Strain", "Pressure (kPa)", "Force (N)", "Time (s)"];
+cz = ["b", "m", "r"];
+xAx = [4, 4, 1, 2];    %which subplots will use which varS for x-axis 
 % 10mm 13cm
+for i = 1:length(Cut)
     figure
-    subplot 121
-    plot(T(:,4,i),T(:,3,i),'b.');
-    xlabel('time (s)')
-    ylabel('Force(N)')
-    title('10mm 13cm Pressurizing - all kinks')
-    subplot 122
-    plot(AllBPA10mm13cm_DP(:,3),AllBPA10mm13cm_DP(:,1),'r.');
-    title('10mm 13cm Depressurizing - all kinks')
-    xlabel('time (s)')
-    ylabel('Force(N)')
-% 10mm 23cm
-    figure
-    subplot 121
-    plot(AllBPA10mm23cm(:,4),AllBPA10mm23cm(:,3),'bo');
-    subplot 122
-    plot(AllBPA10mm23cm_DP(:,3),AllBPA10mm23cm_DP(:,1),'ro');
-    title('10mm23cm all kinks')
-    xlabel('Time(s)')
-    ylabel('Force(N)')
-    grid on
-% 10mm 27cm
-    figure
-    subplot 121
-    plot(AllBPA10mm27cm(:,4),AllBPA10mm27cm(:,3),'bo');
-    subplot 122
-    plot(AllBPA10mm27cm_DP(:,3),AllBPA10mm27cm_DP(:,1),'ro');
-    title('10mm27cm all kinks')
-    xlabel('Time(s)')
-    ylabel('Force(N)')
-    grid on
-%10mm 29cm
-    figure
-    subplot 121
-    plot(AllBPA10mm29cm(:,4),AllBPA10mm29cm(:,3),'bo');
-    subplot 122
-    plot(AllBPA10mm29cm_DP(:,3),AllBPA10mm29cm_DP(:,1),'ro');
-    title('10mm29cm all kinks')
-    xlabel('Time(s)')
-    ylabel('Force(N)')
-    grid on
-
-%10mm 30cm
-    figure
-    subplot 121
-    plot(AllBPA10mm30cm(:,4),AllBPA10mm30cm(:,3),'bo');
-    subplot 122
-    plot(AllBPA10mm30cm_DP(:,3),AllBPA10mm30cm_DP(:,1),'ro');
-    title('10mm30cm all kinks')
-    xlabel('Time(s)')
-    ylabel('Force(N)')
-    grid on
-
-
-
-
+    for a = 1:length(xAx)
+    subplot(2,2,a)
+    hold on
+    x = xAx(a);
+    y = 3;
+    for j = 1:length(li{i})
+        for k = 1:length(test)
+            xx = T{i,j,k}(:,x);
+            yy = T{i,j,k}(:,y);
+            str1 = sprintf('%.2f rel. strain, test %s ',relE{i,j},test(k));
+            scatter(xx,yy,[],cz(k),'DisplayName',str1);
+        end
+    end
+    hold off
+    xlabel(varS(x))
+    ylabel(varS(y))
+    str2 = sprintf('%.1fcm l_{rest}, %s vs. %s - all kinks',lo(i),varS(x),varS(y));
+    title(str2)
+    lgd = legend;
+    end
+end
+    
